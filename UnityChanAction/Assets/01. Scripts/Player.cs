74dd3dae70d38c3be2066c1ce70a152f1bc24c50
kooa <kooa77@gmail.com>
2018-03-03 15:21:46 +0900

    유니티 장의 3D 액션 탄막 게임 1. 유니티 짱 이동 및 회전

---
+++ UnityChanAction/Assets/01. Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdateRotate();
+        UpdateMove();
+	}
+
+
+    // Rotate
+
+    float _rotationY = 0.0f;
+
+    void UpdateRotate()
+    {
+        float rateSpeed = 360.0f;
+        float addRotationY = Input.GetAxis("Mouse X") * rateSpeed;
+        _rotationY += (addRotationY * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(0.0f, _rotationY, 0.0f);
+    }
+
+
+    // Move
+
+    void UpdateMove()
+    {
+        Vector3 addPosition = Vector3.zero;
+
+        Vector3 inputVertical = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));
+        if( 0.0f < inputVertical.z )
+        {
+            addPosition.z = MoveOffset(5.0f);
+        }
+        else if(inputVertical.z < 0.0f)
+        {
+            addPosition.z = MoveOffset(-2.0f);
+        }
+
+        Vector3 inputHorizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        if(0.0f < inputHorizontal.x)
+        {
+            addPosition.x = MoveOffset(3.0f);
+        }
+        else if( inputHorizontal.x < 0.0f )
+        {
+            addPosition.x = MoveOffset(-3.0f);
+        }
+
+        transform.position += (transform.rotation * addPosition);
+    }
+
+    float MoveOffset(float moveSpeed)
+    {
+        return (moveSpeed * Time.deltaTime);
+    }
+}
